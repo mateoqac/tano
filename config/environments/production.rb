b5a8f631cd62b1f53fc30f96f85602306d65d4c6
Mateo Andres <mate.qac@gmail.com>
2022-07-24 18:06:21 +0200

    Change mailer config for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,18 +67,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'ciudadaniaitalia.com' }
 
-  mailertogo_host     = ENV.fetch('MAILERTOGO_SMTP_HOST')
-  mailertogo_port     = ENV.fetch('MAILERTOGO_SMTP_PORT', 587)
-  mailertogo_user     = ENV.fetch('MAILERTOGO_SMTP_USER')
-  mailertogo_password = ENV.fetch('MAILERTOGO_SMTP_PASSWORD')
-  mailertogo_domain   = ENV.fetch('MAILERTOGO_DOMAIN', 'ciudadaniaitalia.com')
-
   config.action_mailer.smtp_settings = {
-    address: mailertogo_host,
-    port: mailertogo_port,
-    user_name: mailertogo_user,
-    password: mailertogo_password,
-    domain: mailertogo_domain,
+    address: 'smpt.sendgrid.net',
+    port: 587,
+    user_name: 'apikey',
+    password: ENV.fetch('SENDGRID_API_KEY'),
+    domain: 'ciudadaniaitalia.com',
     authentication: :plain,
     enable_starttls_auto: true
   }
