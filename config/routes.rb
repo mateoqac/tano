da57afa2e5263e96afd48cd05d00c4306ef6db80
Mateo Andres <mate.qac@gmail.com>
2022-12-03 19:37:50 +0100

    Fix error with routes default format

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@
 
 Rails.application.routes.draw do
   root 'home#index'
-  defaults format: :json do
-    post 'login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
-    get 'login', to: 'sessions#new'
+  # defaults format: :json do
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'login', to: 'sessions#new'
 
-    post 'sign_up', to: 'login_users#create'
-    get 'sign_up', to: 'login_users#new'
-    resources :login_user_confirmations, only: %i[create new edit], param: :confirmation_token
+  post 'sign_up', to: 'login_users#create'
+  get 'sign_up', to: 'login_users#new'
+  resources :login_user_confirmations, only: %i[create new edit], param: :confirmation_token
 
-    post '/subscriber_user', to: 'subscriber_user#create', as: :subscriber_user
+  post '/subscriber_user', to: 'subscriber_user#create', as: :subscriber_user
 
-    post '/question/:id/yes', to: 'home#answer_yes', as: 'answer_yes'
-    post '/question/:id/no', to: 'home#answer_no', as: 'answer_no'
-    get '/reset_form', to: 'home#reset_form'
+  post '/question/:id/yes', to: 'home#answer_yes', as: 'answer_yes'
+  post '/question/:id/no', to: 'home#answer_no', as: 'answer_no'
+  get '/reset_form', to: 'home#reset_form'
 
-    get '/start_checking', to: 'home#start_checking'
-  end
+  get '/start_checking', to: 'home#start_checking'
+  # end
 end
