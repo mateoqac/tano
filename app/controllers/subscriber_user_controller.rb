7365b08673967923573959fd01fd2fd2497ec6bc
Mateo Andres <mate.qac@gmail.com>
2022-12-04 10:10:50 +0100

    Fix subscriber controller

--- app/controllers/subscriber_user_controller.rb
+++ app/controllers/subscriber_user_controller.rb
@@ -2,9 +2,7 @@
 
 class SubscriberUserController < ApplicationController
   def create
-    subscriber_user = SubscriberUser.new(subscriber_user_params)
-
-    return unless subscriber_user.save
+    subscriber_user = SubscriberUser.create(subscriber_user_params)
 
     SubscriberUserMailer.subscribe(subscriber_user.email).deliver
     redirect_to root_path, info: 'Gracias por suscribirte, en breve recibiras un correo con mas informacion'
